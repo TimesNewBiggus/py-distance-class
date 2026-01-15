df76f0cb1f498194e388066587c674ad82ffc684
Dmytro Ponomarenko <dmytro.g.ponomarenko@gmail.com>
2026-01-15 18:43:28 +0200

    Solution redacted 3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 class Distance:
-    def __init__(self, km: int | float ) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,12 +9,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int | float ) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(km=self.km + other.km)
         return Distance(km=self.km + other)
 
-    def __iadd__(self, other: Distance | int | float ) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -48,7 +48,7 @@ class Distance:
             return self.km <= other.km
         return self.km <= other
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
